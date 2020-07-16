fdfaa130b48d380c04b9f8fdf425bb503aef0f99
shinyay <shinya.com@gmail.com>
2020-07-17 00:06:14 +0900

    Modify valiable syntax

--- src/main/kotlin/com/google/shinyay/controller/ConfigClient.kt
+++ src/main/kotlin/com/google/shinyay/controller/ConfigClient.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ConfigClient(@Value(value = "\$config.repo") val repo: String) {
+class ConfigClient(@Value(value = "\${config.repo}") val repo: String) {
 
     @GetMapping("/config/test")
     fun test() = "Remote Repository info is $repo"

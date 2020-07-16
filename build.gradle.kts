d5ccaed8082fabc4573e0180c65b4decf42ba60f
shinyay <shinya.com@gmail.com>
2020-07-17 00:04:57 +0900

    Add Web dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,8 @@ repositories {
 extra["springCloudVersion"] = "Hoxton.SR6"
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")

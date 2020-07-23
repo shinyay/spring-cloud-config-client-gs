4c2fbaf94fc04c11b0809c075e10cec8840357d2
shinyay <shinya.com@gmail.com>
2020-07-23 22:25:16 +0900

    Add enable DiscoveryClient for Eureka Server

--- src/main/kotlin/com/google/shinyay/controller/ConfigClient.kt
+++ src/main/kotlin/com/google/shinyay/controller/ConfigClient.kt
@@ -1,10 +1,12 @@
 package com.google.shinyay.controller
 
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@EnableDiscoveryClient
 class ConfigClient(@Value(value = "\${config.repo}") val repo: String) {
 
     @GetMapping("/config/test")
